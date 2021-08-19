fa4be1cae40e6bae25e7535485f623b6504a4168
GaurangMistry <dotnetplayer@gmail.com>
2021-08-19 14:24:50 +0530

    End of section 13

--- client-app/src/features/home/HomePage.tsx
+++ client-app/src/features/home/HomePage.tsx
@@ -1,28 +1,41 @@
+import { observer } from "mobx-react-lite";
 import React from "react";
 import { Link } from "react-router-dom";
 import { Button, Container, Header, Image, Segment } from "semantic-ui-react";
+import { useStore } from "../../app/stores/store";
+import LoginForm from "../users/LoginForm";
+import RegisterForm from "../users/RegisterForm";
 
-export default function HomePge(){
+export default observer(function HomePge(){
+    const {userStore, modalStore} = useStore();
     return(
-        // <Container style={{marginTop: '7em'}}>
-        //     <h1>Home Page</h1>
-        //     <h3>
-        //         Go to <Link to='/activities'>Activities</Link>
-        //     </h3>
-        // </Container>
-
         <Segment inverted textAlign="center" vertical className='masthead'>
             <Container text>
                 <Header as='h1' inverted>
                     <Image size='massive' src='/assets/logo.png' alt='logo' style={{marginBottom: 12}} />
                     Reactivities
                 </Header>
-                <Header as='h2' inverted content='Welcome to Activities' />
-                <Button as={Link} to='/activities' size='huge' inverted>
-                    Take me to Activities
-                </Button>
+                {userStore.isLoggedIn ? (
+                    <>
+                        <Header as='h2' inverted content='Welcome to Activities' />
+                        <Button as={Link} to='/activities' size='huge' inverted>
+                            Go to Activities
+                        </Button>
+                    </>
+
+                ) : (
+                    <>
+                         <Button  onClick={() => modalStore.openModal(<LoginForm />)}  size='huge' inverted>
+                            Login
+                        </Button>
+                        <Button  onClick={() => modalStore.openModal(<RegisterForm />)}  size='huge' inverted>
+                            Register
+                        </Button>
+                    </>
+                )}
+                
             </Container>
 
         </Segment>
     )
-}
+}) 
